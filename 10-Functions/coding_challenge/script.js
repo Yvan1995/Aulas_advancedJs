53f6ee271233d120736c0c5ffcd3c6937eaa9685
Yvan Rondon <yvanneto1995@gmail.com>
2022-11-19 18:39:19 -0400

    commit teste, chalenge 2

--- 10-Functions/coding_challenge/script.js
+++ 10-Functions/coding_challenge/script.js
@@ -5,6 +5,11 @@ Your tasks:
 1. Take the IIFE below and at the end of the function, attach an event listener that
 changes the color of the selected h1 element ('header') to blue, each time
 the body element is clicked. Do not select the h1 element again!
+
+(1. Pegue o IIFE abaixo e ao final da função, anexe um event listener que
+muda a cor do elemento h1 selecionado ('cabeçalho') para azul, toda vez
+o elemento do corpo é clicado. Não selecione o elemento h1 novamente!)
+
 2. And now explain to yourself (or someone around you) why this worked! Take all
 the time you need. Think about when exactly the callback function is executed,
 and what that means for the variables involved in this example.
