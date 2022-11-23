a62b9571082ea691cd3dfbc3b6b291fe43e4a844
Yvan Rondon <yvanneto1995@gmail.com>
2022-11-23 18:30:48 -0400

    at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,35 +75,55 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-// SLICE
-let arr = ['a', 'b', 'c', 'd', 'e' ];
-console.log(arr.slice(2));
-console.log(arr.slice(2,4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
+/**
+ * 
+ // SLICE
+ let arr = ['a', 'b', 'c', 'd', 'e' ];
+ console.log(arr.slice(2));
+ console.log(arr.slice(2,4));
+ console.log(arr.slice(-2));
+ console.log(arr.slice(-1));
+ console.log(arr.slice(1, -2));
+ 
+ // SPLICE
+ 
+ // console.log(arr.splice(2));
+ arr.splice(-1);
+ console.log(arr);
+ arr.splice(1,2);
+ console.log(arr);
+ 
+ // REVERSE
+ 
+ arr = ['a', 'b', 'c', 'd', 'e' ];
+ const arr2 = ['j', 'i', 'h', 'g', 'f'];
+ console.log(arr2.reverse());
+ console.log(arr2);
+ 
+ // CONCAT
+ 
+ const letters = arr.concat(arr2);
+ console.log(letters);
+ console.log([...arr, ...arr2]);
+ 
+ // JOIN
+ 
+ console.log(letters.join(' - '));
+ */
 
-// SPLICE
 
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1,2);
-console.log(arr);
+// aula 143
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
 
-// REVERSE
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr.at(-2));
 
-arr = ['a', 'b', 'c', 'd', 'e' ];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// at é iteravel com strings
 
-// CONCAT
-
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// JOIN
-
-console.log(letters.join(' - '));
+console.log('yvan'.at(0));
+console.log('yvan'.at(-1));
